193f58d2b95f6d5ab3f91476ec0df0ee84718930
Vladimir <125801213+porazitelny@users.noreply.github.com>
2025-11-07 08:48:18 +0000

    laba2

--- android/app/src/main/kotlin/com/example/mobilki/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mobilki
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export PATH="$PATH:/workspaces/$(basename $GITHUB_REPOSITORY)/flutter/bin"
-flutter pub get
-flutter run -d web-server --web-port 3000 --web-hostname 0.0.0.0
